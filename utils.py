2e7fd15b22c7b638c3dee978aefae39b8a7b4f38
HAMarat <smile56680@gmail.com>
2022-10-27 23:15:38 +0300

    12

--- utils.py
+++ utils.py
@@ -1,26 +1,26 @@
 import json
-from typing import List, Dict
+from typing import Union
 
 
-def load_candidates(filename: str) -> List[Dict]:
+def load_candidates(filename: str) -> list[dict]:
     with open(filename, 'r', encoding='utf-8') as file:
         candidates_data = json.load(file)
         return candidates_data
 
 
-def get_all(candidates) -> list[dict]:
+def get_all(candidates: list[dict]) -> list[dict]:
     candidates_name = []
     for candidates_dict in candidates:
         candidates_name.append(candidates_dict['name'])
     return candidates_name
 
 
-def get_by_pk(candidates, pk):
-    if pk.isdigit():
-        for candidates_dict in candidates:
-            if candidates_dict["pk"] == int(pk):
-                return candidates_dict
-    return None
+def get_by_pk(candidates, pk) -> Union[list[dict], None]:
+    if not pk.isdigit():
+        return None
+    for candidates_dict in candidates:
+        if candidates_dict["pk"] == int(pk):
+            return candidates_dict
 
 
 def get_by_skill(candidates, skill_name):
@@ -28,5 +28,6 @@ def get_by_skill(candidates, skill_name):
     for candidate_dict in candidates:
         if skill_name.lower() in candidate_dict['skills'].lower():
             candidates_by_skill.append(candidate_dict)
-            return candidates_by_skill
+    if candidates_by_skill:
+        return candidates_by_skill
     return None
