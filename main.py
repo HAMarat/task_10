b7e2d9d429829e7068a29baf17357322ea93adf6
Huawei_station <smile56680@gmail.com>
2022-10-28 17:21:06 +0300

    4 End candidate page

--- main.py
+++ main.py
@@ -10,35 +10,33 @@ app = Flask(__name__)
 candidates_data: list[dict] = load_candidates(JSON_DATA)
 
 
-def main():
+@app.route('/')
+def candidates() -> str:
     """
-    Запускаем программу только если она запущена напрямую из файла main.py
+    Возвращаем данные всех кандидатов по адресу "/"
     """
-    @app.route('/')
-    def candidates() -> str:
-        """
-        Возвращаем данные всех кандидатов по адресу "/"
-        """
-        candidates_name = get_all(candidates_data)
-        return f"<p align='center'>{''.join(candidates_name)}</>"
+    candidates_name = get_all(candidates_data)
+    return "".join(candidates_name)
 
-    @app.route('/candidates/<pk_number>')
-    def candidate_by_pk(pk_number: str) -> str:
-        """
-        Выводим данные кандидата по адресу "/candidates/<pk_number>"
-        """
-        if not pk_number.isdigit():
-            return "Пользователь с таким 'pk' отсутствует"
-        candidate_pk_number = get_by_pk(candidates_data, pk_number)
-        return candidate_pk_number
 
-    @app.route('/skills/<skill>')
-    def candidates_by_skill(skill: str) -> str:
-        """
-        Выводим данные кандидатов по адресу "/skills/<skill>"
-        """
-        list_candidates_by_skill = get_by_skill(candidates_data, skill)
-        return ''.join(list_candidates_by_skill)
+@app.route('/candidates/<pk_number>')
+def candidate_by_pk(pk_number: str) -> str:
+    """
+    Выводим данные кандидата по адресу "/candidates/<pk_number>"
+    """
+    if not pk_number.isdigit():
+        return "Пользователь с таким 'pk' отсутствует"
+    candidate_pk_number = get_by_pk(candidates_data, pk_number)
+    return candidate_pk_number
+
+
+@app.route('/skills/<skill>')
+def candidates_by_skill(skill: str) -> str:
+    """
+    Выводим данные кандидатов по адресу "/skills/<skill>"
+    """
+    list_candidates_by_skill = get_by_skill(candidates_data, skill)
+    return ''.join(list_candidates_by_skill)
 
 
 if __name__ == "__main__":
