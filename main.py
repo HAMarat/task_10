d3784f3377448a7e27fd383e83c5d8c6e9ba17dc
Huawei_station <smile56680@gmail.com>
2022-10-28 16:38:40 +0300

    3 End candidate page

--- main.py
+++ main.py
@@ -1,48 +1,44 @@
 from flask import Flask
-from utils import load_candidates, get_by_pk, get_by_skill
+from utils import load_candidates, get_all, get_by_pk, get_by_skill
 
+# ссылка на файл с данными кандидатов
 JSON_DATA = 'candidates.json'
 
 app = Flask(__name__)
-candidates_data = load_candidates(JSON_DATA)
+
+# Загружаем данные кандидатов
+candidates_data: list[dict] = load_candidates(JSON_DATA)
 
 
-@app.route('/')
-def candidates():
-    list_data = []
-    for candidate_data in candidates_data:
-        list_data.append(f"<pre>Имя кандидата - {candidate_data['name']}\n"
-                         f"Позиция кандидата - {candidate_data['position']}\n"
-                         f"Навыки: {candidate_data['skills']}</pre>")
-    return ''.join(list_data)
+def main():
+    """
+    Запускаем программу только если она запущена напрямую из файла main.py
+    """
+    @app.route('/')
+    def candidates() -> str:
+        """
+        Возвращаем данные всех кандидатов по адресу "/"
+        """
+        candidates_name = get_all(candidates_data)
+        return f"<p align='center'>{''.join(candidates_name)}</>"
 
+    @app.route('/candidates/<pk_number>')
+    def candidate_by_pk(pk_number: str) -> str:
+        """
+        Выводим данные кандидата по адресу "/candidates/<pk_number>"
+        """
+        if not pk_number.isdigit():
+            return "Пользователь с таким 'pk' отсутствует"
+        candidate_pk_number = get_by_pk(candidates_data, pk_number)
+        return candidate_pk_number
 
-@app.route('/candidates/<pk_number>')
-def candidate_by_pk(pk_number: str):
-    candidate_pk_number = get_by_pk(candidates_data, pk_number)
-    if candidate_pk_number:
-        url_pk = candidate_pk_number['picture']
-        candidate_pk_return = (f"<img src='{url_pk}'>"
-                               f"<pre>Имя кандидата - {candidate_pk_number['name']}\n"
-                               f"Позиция кандидата - {candidate_pk_number['position']}\n"
-                               f"Навыки: {candidate_pk_number['skills']}</pre>")
-        return candidate_pk_return
-    return "Пользователь с таким 'pk' отсутствует"
-
-
-@app.route('/skills/<skill>')
-def candidates_by_skill(skill: str):
-    list_candidates_by_skill = []
-    candidates_by_skill_dict = get_by_skill(candidates_data, skill)
-    if candidates_by_skill_dict:
-        for candidate_skill in candidates_by_skill_dict:
-            candidate_skill_return = (f"<pre>Имя кандидата - {candidate_skill['name']}\n"
-                                      f"Позиция кандидата - {candidate_skill['position']}\n"
-                                      f"Навыки: {candidate_skill['skills']}</pre>")
-            list_candidates_by_skill.append(candidate_skill_return)
-        if ''.join(list_candidates_by_skill):
-            return ''.join(list_candidates_by_skill)
-    return "Кандидатов с таким навыком нет"
+    @app.route('/skills/<skill>')
+    def candidates_by_skill(skill: str) -> str:
+        """
+        Выводим данные кандидатов по адресу "/skills/<skill>"
+        """
+        list_candidates_by_skill = get_by_skill(candidates_data, skill)
+        return ''.join(list_candidates_by_skill)
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -3,31 +3,53 @@ from typing import Union
 
 
 def load_candidates(filename: str) -> list[dict]:
+    """
+    Загружает данные из файла
+    """
     with open(filename, 'r', encoding='utf-8') as file:
         candidates_data = json.load(file)
         return candidates_data
 
 
-def get_all(candidates: list[dict]) -> list[dict]:
+def get_all(candidates: list[dict]) -> list[str]:
+    """
+    Возвращает данные всех кандидатов
+    """
     candidates_name = []
-    for candidates_dict in candidates:
-        candidates_name.append(candidates_dict['name'])
+    for candidate_dict in candidates:
+        candidate_name = (f"<pre>Имя кандидата - {candidate_dict['name']}\n"
+                          f"Позиция кандидата - {candidate_dict['position']}\n"
+                          f"Навыки: {candidate_dict['skills']}</pre>")
+        candidates_name.append(candidate_name)
     return candidates_name
 
 
-def get_by_pk(candidates, pk) -> Union[list[dict], None]:
-    if not pk.isdigit():
-        return None
-    for candidates_dict in candidates:
-        if candidates_dict["pk"] == int(pk):
-            return candidates_dict
+def get_by_pk(candidates: list[dict], pk_number: str) -> str:
+    """
+    Возвращает данные кандидата по указанному "pk"
+    """
+    for candidate_dict in candidates:
+        if candidate_dict["pk"] == int(pk_number):
+            url_pk = candidate_dict['picture']
+            candidate_by_pk = (f"<img src='{url_pk}'>"
+                               f"<pre>Имя кандидата - {candidate_dict['name']}\n"
+                               f"Позиция кандидата - {candidate_dict['position']}\n"
+                               f"Навыки: {candidate_dict['skills']}</pre>")
+            return candidate_by_pk
+    return "Пользователь с таким 'pk' отсутствует"
 
 
-def get_by_skill(candidates, skill_name):
-    candidates_by_skill = []
+def get_by_skill(candidates: list[dict], skill_name: str) -> Union[list[str], str]:
+    """
+    Возвращает данные кандидатов с указанными навыками
+    """
+    list_candidates_by_skill = []
     for candidate_dict in candidates:
         if skill_name.lower() in candidate_dict['skills'].lower():
-            candidates_by_skill.append(candidate_dict)
-    if candidates_by_skill:
-        return candidates_by_skill
-    return None
+            candidate_by_skill = (f"<pre>Имя кандидата - {candidate_dict['name']}\n"
+                                  f"Позиция кандидата - {candidate_dict['position']}\n"
+                                  f"Навыки: {candidate_dict['skills']}</pre>")
+            list_candidates_by_skill.append(candidate_by_skill)
+    if list_candidates_by_skill:
+        return list_candidates_by_skill
+    return "Кандидатов с таким навыком нет"
